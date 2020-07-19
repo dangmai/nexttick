6d6f8f9707135c10619c2e63c7a4148089085995
Dang Mai <contact@dangmai.net>
2020-07-19 16:59:05 -0400

    Use Redux store in Control Panel story

--- src/components/ControlPanel/ControlPanel.stories.tsx
+++ src/components/ControlPanel/ControlPanel.stories.tsx
@@ -1,38 +1,37 @@
 import React from "react";
-import { ControlPanel } from "./ControlPanel";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider as ReduxProvider } from "react-redux";
+
+import { ConnectedControlPanel } from "./ControlPanel";
+import rootReducer from "../../rootReducer";
+
+import "bootstrap/dist/css/bootstrap.css";
+import "argon-design-system-react/src/assets/scss/argon-design-system-react.scss";
+import "argon-design-system-react/src/assets/vendor/font-awesome/css/font-awesome.css";
+
+const store = configureStore({
+  reducer: rootReducer,
+});
 
 export default {
-  component: ControlPanel,
+  component: ConnectedControlPanel,
   title: "ControlPanel",
   decorators: [
-    (storyFn: typeof ControlPanel) => (
-      <div style={{ position: "relative", width: "1920px" }}>
-        <img
-          alt="CSGO"
-          src="https://i.imgur.com/OS0EHHg.jpg"
-          style={{ maxWidth: "100%" }}
-        />
-        <div style={{ position: "absolute", bottom: 0, width: "inherit" }}>
-          {storyFn({})}
+    (storyFn: typeof ConnectedControlPanel, context: any) => (
+      <ReduxProvider store={store}>
+        <div style={{ position: "relative", width: "1920px" }}>
+          <img
+            alt="CSGO"
+            src="https://i.imgur.com/OS0EHHg.jpg"
+            style={{ maxWidth: "100%" }}
+          />
+          <div style={{ position: "absolute", bottom: 0, width: "inherit" }}>
+            {storyFn()}
+          </div>
         </div>
-      </div>
+      </ReduxProvider>
     ),
   ],
 };
 
-const handleClick = () => {
-  console.log("A button was clicked");
-};
-
-const handleSpeedChange = (speed: number) => {
-  console.log(`New speed detected: ${speed}`);
-};
-
-export const defaultControlPanel = () => (
-  <ControlPanel
-    handleNextRound={handleClick}
-    handlePreviousRound={handleClick}
-    handlePlayPause={handleClick}
-    handleSpeedChange={handleSpeedChange}
-  />
-);
+export const defaultControlPanel = () => <ConnectedControlPanel />;
