551a344199e65f52bedb7c6a9d3101382731c600
Dang Mai <contact@dangmai.net>
2020-07-23 01:25:43 -0400

    Fix linting issue

--- src/components/Overlay/Overlay.stories.tsx
+++ src/components/Overlay/Overlay.stories.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, useEffect } from "react";
+import React, { FunctionComponent } from "react";
 import { withKnobs, boolean } from "@storybook/addon-knobs";
 
 import { configureStore } from "@reduxjs/toolkit";
